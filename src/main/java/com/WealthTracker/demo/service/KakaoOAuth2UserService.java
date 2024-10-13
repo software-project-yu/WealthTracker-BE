8e34dbc7077634a0c2a8c1f7e29e2195bbb4a484
Park Jae Seong <timer973@naver.com>
2024-10-13 13:01:14 +0900

    ✨feat:Kakao Login시 저장되는 정보(id, email, nickname)를 JWT로 생성

--- src/main/java/com/WealthTracker/demo/service/KakaoOAuth2UserService.java
+++ src/main/java/com/WealthTracker/demo/service/KakaoOAuth2UserService.java
@@ -27,19 +27,26 @@ public class KakaoOAuth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         Map<String, Object> attributes = oAuth2User.getAttributes();
+        // 사용자 정보 추출
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
 
-        // 카카오에서 받은 사용자 정보
-        CustomUserInfoDTO userInfo = CustomUserInfoDTO.builder()
+        String nickname = properties.get("nickname").toString(); //** 카카오 로그인 한 유저의 이름을 저장
+        String email = kakaoAccount.get("email").toString(); //** 카카오 로그인 한 유저의 이메일을 저장
+
+        CustomUserInfoDTO userInfo = CustomUserInfoDTO.builder() //** 사용자 정보를 DTO에 저장 후 Builder로 id, email, name 저장
                 .userId(Long.parseLong(attributes.get("id").toString()))
+                .email(email)
+                .name(nickname) // nickname이 사실상 name임
                 .build();
 
-        // JWT 토큰 생성
-        String token = jwtUtil.createAccessToken(userInfo);
+
+        String token = jwtUtil.createAccessToken(userInfo); //** 해당 카카오 로그인 유저의 정보로 서비스 자체의 토큰 생성
 
         // 생성된 JWT 토큰 출력
         log.info("JWT Token: {}", token);
 
-        // 클라이언트에 JWT 토큰 전달 (필요에 따라 response에 추가하거나 세션에 저장)
+        // 클라이언트에 JWT 토큰 전달
         httpSession.setAttribute("login_info", attributes);
         httpSession.setAttribute("jwt_token", token);
 
