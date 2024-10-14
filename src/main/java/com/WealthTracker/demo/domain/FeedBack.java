b6cb4a6b1069802d3028ce2f4948654583172453
tkv00 <tkv0098@gmail.com>
2024-10-14 17:17:52 +0900

    ✨feat:FeedBack 엔티티 구현

---
+++ src/main/java/com/WealthTracker/demo/domain/FeedBack.java
@@ -0,0 +1,31 @@
+package com.WealthTracker.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Feedback")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FeedBack {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedId;
+
+    @Lob
+    private String content;
+
+    @DateTimeFormat
+    private LocalDateTime createdAt;
+
+    //유저랑 N 대 1
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private User user;
+}
