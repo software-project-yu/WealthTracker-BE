455a0ede3a78f0442f04b40009ddbc74da66d83f
Park Jae Seong <152384393+pjs1710@users.noreply.github.com>
2024-11-24 18:22:39 +0900

    Update SignupRequestDTO.java

--- src/main/java/com/WealthTracker/demo/DTO/SignupRequestDTO.java
+++ src/main/java/com/WealthTracker/demo/DTO/SignupRequestDTO.java
@@ -16,20 +16,6 @@ import lombok.NoArgsConstructor;
 public class SignupRequestDTO {
 
     @NotNull
-    @Size(min = 1, max = 50)
-    @Email
-    private String email;
-
-    @NotNull
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z\\d!@#$%^&*]{8,15}$")
-    //영어 + 숫자 + 특수문자 8~15자
-    private String password;
-
-    @NotNull
-    @Size(min = 1, max = 20)
-    private String name;
-
-    @NotNull
     //6-15자 영어 숫자
     @Pattern(regexp = "^[a-zA-Z0-9]{6,15}$")
     @Pattern(regexp = "^[A-Za-z0-9_\\\\.\\\\-]+@[A-Za-z0-9\\\\-]+\\\\.[A-Za-z0-9\\\\-]+$", message = "알맞은 이메일 형식을 입력.")
