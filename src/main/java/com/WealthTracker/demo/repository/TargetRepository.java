e469ddd5d19e4d300b9cc5b12b7312d7e8077976
tkv00 <tkv0098@gmail.com>
2024-11-28 00:38:02 +0900

    🐛fix:null값 반환 예외처리

--- src/main/java/com/WealthTracker/demo/repository/TargetRepository.java
+++ src/main/java/com/WealthTracker/demo/repository/TargetRepository.java
@@ -11,13 +11,13 @@ public interface TargetRepository extends JpaRepository<Target, Long> {
     Optional<Target> findByTargetIdAndUserUserId(Long targetId, Long userId);
 
     //월별 목표 금액 리턴 -> 예외 발생하기는 하지만 목표 시작날짜와 끝나는 날짜 모두 같은 월에 해당하먄 리탄
-    @Query("select sum(t.targetAmount) from Target t "+
+    @Query("select coalesce(sum(t.targetAmount),0) from Target t "+
             "where :month = month(t.startDate) "+
             "and :month = month(t.endDate)")
     int getAllTargetAmountByMonth(@Param("month")int month);
 
     //월별 저축 금액 리턴
-    @Query("select sum(t.savedAmount) from Target t "+
+    @Query("select coalesce(sum(t.savedAmount),0)  from Target t "+
             "where :month = month(t.startDate) "+
             "and :month = month(t.endDate)")
     int getAllSavedAmountByMonth(@Param("month")int month);
